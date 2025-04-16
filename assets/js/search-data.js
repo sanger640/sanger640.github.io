fd3bcd541dad3519429b22c09207df57eeee55e6
sanger640 <alimohammed2510@gmail.com>
2025-04-16 12:16:36 +0000

    Deploying to gh-pages from @ sanger640/sanger640.github.io@e8158ece26b5de1c8d825ab10d294c209863f799 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -117,25 +117,11 @@ ninja.data = [{
           window.open("mailto:%61%6C%69%6B%61%73%69%6D.%62%75%64%68%77%61%6E%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/alikasim-budhwani", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
