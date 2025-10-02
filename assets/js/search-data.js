95d2e5e397ea8d782397e3e17faa1c567859d52a
sanger640 <alimohammed2510@gmail.com>
2025-10-02 16:30:55 +0000

    Deploying to gh-pages from @ sanger640/sanger640.github.io@d160820757bb3fc04765272fef1841589af80124 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-policy-optimization-and-actor-critic-methods",
+        },{id: "post-hamilton-jacobi-reachability",
+        
+          title: "Hamilton Jacobi Reachability",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/hj/";
+          
+        },
+      },{id: "post-policy-optimization-and-actor-critic-methods",
         
           title: "Policy Optimization and Actor Critic Methods",
         
