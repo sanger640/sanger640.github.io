d91ae0cc2d6d0cc0d0fb53a64cec87a7888722cc
sanger640 <alimohammed2510@gmail.com>
2025-10-25 16:38:12 +0000

    Deploying to gh-pages from @ sanger640/sanger640.github.io@2d4ddc9dfd422385dca7b5360d4d32382fea3b26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-time-reversibility",
+        },{id: "post-imitation-learning",
+        
+          title: "Imitation Learning",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/imit/";
+          
+        },
+      },{id: "post-time-reversibility",
         
           title: "Time Reversibility",
         
