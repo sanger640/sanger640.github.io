6b4c68b98b9992d21c025d243a56a8430fd96177
sanger640 <alimohammed2510@gmail.com>
2025-11-03 20:12:49 +0000

    Deploying to gh-pages from @ sanger640/sanger640.github.io@df029de776c370addcba7559492f129a87742c65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-imitation-learning",
+        },{id: "post-nonlinear-dynamics-and-control-i",
+        
+          title: "Nonlinear Dynamics and Control I",
+        
+        description: "One Dimensional Flows",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/nld/";
+          
+        },
+      },{id: "post-imitation-learning",
         
           title: "Imitation Learning",
         
