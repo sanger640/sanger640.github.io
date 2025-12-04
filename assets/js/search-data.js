556ebbe66f676945d8a8db672479030fce834f8c
sanger640 <alimohammed2510@gmail.com>
2025-12-04 04:27:28 +0000

    Deploying to gh-pages from @ sanger640/sanger640.github.io@ebd019b571fdd795b93219a946f909e254cdbcc3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "Resume",
+          description: "Redirecting to resume...",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "post-nonlinear-dynamics-and-control-i",
         
